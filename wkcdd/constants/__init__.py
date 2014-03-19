98ad64ec8f09acc5e79e9e84d70d144433c60b22
royrutto <royrutto@gmail.com>
2014-03-19 16:04:51 +0300

    RR: create constants file

---
+++ wkcdd/constants/__init__.py
@@ -0,0 +1,15 @@
+XFORM_ID = '_xform_id_string'
+COMMUNITY_NAME = 'p_name'
+PROJECT_CODE = 'perfomance_summary/dc_projects'
+PROJECT_TYPE = 'projecttype'
+COUNTY_COUNTIES = 'county_counties'
+COUNT_SUB_COUNTIES = 'county_sub_counties'
+COUNTY_CONSTITUENCY = 'county_contituency'
+COUNTY = 'county'
+DATE_COMPILED = 'photo_signatures/date_compiled'
+
+PROJECT_REGISTRATION = 'dairy_cows_project_registration3'
+PROJECT_REPORT = 'dairy_cows_project_report'
+
+
+PERFOANCE_INDICATORS =('Excpect')
