19f084331d5c361ffadcefb9ba4a49f95a4be0ff
royrutto <royrutto@gmail.com>
2014-03-20 13:26:04 +0300

    RR: change constants

--- wkcdd/constants/__init__.py
+++ wkcdd/constants/__init__.py
@@ -1,11 +1,14 @@
 XFORM_ID = '_xform_id_string'
-COMMUNITY_NAME = 'p_name'
+
+PROJECT_NAME = 'p_name'
 PROJECT_CODE = 'dc_projects'
 PROJECT_TYPE = 'projecttype'
-COUNTY_COUNTIES = 'county_counties'
-COUNT_SUB_COUNTIES = 'county_sub_counties'
-COUNTY_CONSTITUENCY = 'county_contituency'
-COUNTY = 'county'
+COMMUNITY_NAME = 'county'
+
+COUNTY = 'county_counties'
+SUB_COUNTIES = 'county_sub_counties'
+CONSTITUENCY = 'county_contituency'
+
 DATE_COMPILED = 'photo_signatures/date_compiled'
 GEOLOCATION = 'project_location'
 
