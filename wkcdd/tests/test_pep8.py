b2d75f779c5c5fa97b55881bb169d319cfc42f92
Larry Weya <larryweya@gmail.com>
2014-03-27 12:24:14 +0300

    LW: ignore static directory in pep8 tests

--- wkcdd/tests/test_pep8.py
+++ wkcdd/tests/test_pep8.py
@@ -5,5 +5,5 @@ from subprocess import call
 
 class TestPEP8(unittest.TestCase):
     def test_flake8(self):
-        result = call(['flake8', 'wkcdd'])
+        result = call(['flake8', 'wkcdd', '--exclude', 'wkcdd/static'])
         self.assertEqual(result, 0, "Code is not flake8.")
