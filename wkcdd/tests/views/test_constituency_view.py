0e689d888133f43f00b2f3a22f0c27b5e80fd82d
royrutto <royrutto@gmail.com>
2014-03-31 15:04:22 +0300

    RR: community view functional test

--- wkcdd/tests/views/test_constituency_view.py
+++ wkcdd/tests/views/test_constituency_view.py
@@ -4,6 +4,7 @@ from wkcdd.tests.test_base import (
     FunctionalTestBase
 )
 from wkcdd.views.constituency import ConstituencyView
+from wkcdd.models.constituency import Constituency
 
 
 class TestConstituencyView(IntegrationTestBase):
@@ -12,8 +13,12 @@ class TestConstituencyView(IntegrationTestBase):
         self.request = testing.DummyRequest()
         self.constituency_view = ConstituencyView(self.request)
 
-    def test_constituency_list_all_projects(self):
+    def test_constituency_list_all_communities(self):
         self.setup_test_data()
+        constituency = Constituency.get(Constituency.name == "Kakamega")
+        self.request.context = constituency
+        response = self.constituency_view.list_all_communities()
+        self.assertIsInstance(response['constituency'], Constituency)
 
 
 class TestCommunityViewsFunctional(FunctionalTestBase):
