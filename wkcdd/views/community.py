1acd651b083770809ddcc261245a8c89917a44bb
royrutto <royrutto@gmail.com>
2014-03-31 12:05:34 +0300

    RR: added initial community projects view

--- wkcdd/views/community.py
+++ wkcdd/views/community.py
@@ -2,7 +2,10 @@ from pyramid.view import (
     view_defaults,
     view_config
 )
-from wkcdd.models.location import LocationFactory
+from wkcdd.models.community import Community
+from wkcdd.models.project import Project
+from wkcdd import constants
+from wkcdd.libs.utils import tuple_to_dict_list
 
 
 @view_defaults(route_name='community')
@@ -11,8 +14,24 @@ class CommunityView(object):
         self.request = request
 
     @view_config(name='',
-                 context=LocationFactory,
-                 renderer='projects_list.jinja2',
+                 context=Community,
+                 renderer='community_projects_list.jinja2',
                  request_method='GET')
     def list_all_projects(self):
-        pass
+        community = self.request.context
+        projects = community.projects
+        constituency = Project.get_constituency(community)
+        sub_county = Project.get_county(constituency)
+        county = Project.get_county(sub_county)
+        locations = {'constituency': constituency,
+                     'sub_county': sub_county,
+                     'county': county}
+
+        return{
+            'community': community,
+            'projects': projects,
+            'locations': locations,
+            'impact_indicator_mapping': tuple_to_dict_list(
+                ('title', 'key'),
+                constants.IMPACT_INDICATOR_REPORT)
+        }
