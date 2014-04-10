e9f40f2a97ab54e329c332d0e84d0059f86dbea8
geoffreymuchai <geoffreymuchai@gmail.com>
2014-04-10 20:04:04 +0300

    GM: Added percentage and number formatting for performance indicators

--- wkcdd/libs/utils.py
+++ wkcdd/libs/utils.py
@@ -1,7 +1,26 @@
-from pyramid.i18n import TranslationStringFactory
+import re
+from pyramid.i18n import TranslationStringFactory, get_localizer
+
+from babel.numbers import format_number
 
 translation_string_factory = TranslationStringFactory('wkcdd')
 
 
 def tuple_to_dict_list(key_tuple, value_tuples):
     return [dict(zip(key_tuple, c)) for c in value_tuples]
+
+
+def format_percent(value, request):
+    value = value if type(value) == float else float(value)
+    localizer = get_localizer(request)
+    return format_number(round(value, 1), locale=localizer.locale_name) + '%'
+
+
+def format_value(value, request):
+    value = value if type(value) == float else float(value)
+    localizer = get_localizer(request)
+    return format_number(value, locale=localizer.locale_name)
+
+
+def number_to_symbol(value):
+    return re.sub('Total|Number', '#', value)
