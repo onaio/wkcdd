6d8010981a0c59dc12e9475c920a25d2c2ca6054
Larry Weya <larryweya@gmail.com>
2014-04-10 15:36:50 +0300

    LW: humanize function

--- wkcdd/libs/utils.py
+++ wkcdd/libs/utils.py
@@ -1,7 +1,20 @@
+import re
+
 from pyramid.i18n import TranslationStringFactory
 
+
 translation_string_factory = TranslationStringFactory('wkcdd')
+humanize_re = re.compile(r"[\W_]+", re.DEBUG)
 
 
 def tuple_to_dict_list(key_tuple, value_tuples):
     return [dict(zip(key_tuple, c)) for c in value_tuples]
+
+
+def humanize(value):
+    """
+    Make value pretty for humans.
+
+    Replace all special characters with spaces.
+    """
+    return humanize_re.sub(" ", value)
---
+++ wkcdd/tests/libs/test_import_project_data.py
@@ -0,0 +1,33 @@
+from wkcdd import constants
+from wkcdd.models.project import Project
+from wkcdd.models.report import Report
+from wkcdd.libs.import_project_data import(
+    fetch_data,
+    add_project,
+    populate_reports_table)
+from wkcdd.tests.test_base import TestBase
+
+
+class TestProjectImport(TestBase):
+    def test_populate_projects_table(self):
+        count = Project.count()
+        form, project_code = constants.PROJECT_REGISTRATION_FORMS[0]
+        add_project(self.project_submission[0], project_code)
+        self.assertEquals(Project.count(), (count + 1))
+
+    def test_populate_report_table_with_valid_project(self):
+        self.setup_test_data()
+        count = Report.count()
+        form, project_code = constants.PROJECT_REPORT_FORMS[1]
+        populate_reports_table([self.report_submission[0]], project_code)
+        self.assertEquals(Report.count(), (count + 1))
+
+    # FIXME should not make actual HTTP Call
+    def test_fetch_data(self):
+        form_id = constants.DAIRY_COWS_PROJECT_REGISTRATION
+        raw_data = fetch_data(form_id)
+        self.assertIsInstance(raw_data, list)
+        self.assertTrue(len(raw_data) > 0)
+        self.assertIsInstance(raw_data[0], dict)
+        self.assertIn(constants.PROJECT_TYPE, raw_data[0])
+        self.assertIn(constants.COUNTY, raw_data[0])
