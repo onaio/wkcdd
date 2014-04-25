d548cf427663b3ccc3ab11ff36b7a5d5f6f82c71
geoffreymuchai <geoffreymuchai@gmail.com>
2014-04-25 16:53:03 +0300

    Fixed failing tests

--- wkcdd/views/county.py
+++ wkcdd/views/county.py
@@ -16,8 +16,7 @@ from wkcdd.models.report import Report
 from wkcdd.models import helpers
 from wkcdd.views.helpers import (
     build_dataset,
-    generate_impact_indicators_for,
-    generate_performance_indicators_for
+    generate_impact_indicators_for
 )
 
 
@@ -28,23 +27,28 @@ class CountyView(object):
     def __init__(self, request):
         self.request = request
 
-    @view_config(name='',
-                 context=LocationFactory,
-                 renderer='counties_list.jinja2',
-                 request_method='GET')
-    def show_all_counties(self):
-        counties = County.all()
-        filter_criteria = Project.generate_filter_criteria()
-        view_by = self.request.GET.get('view_by')
+    def get_location_map(self):
         location_map = {
             'community': self.request.GET.get('community'),
             'constituency': self.request.GET.get('constituency'),
             'sub_county': self.request.GET.get('sub_county'),
             'county': self.request.GET.get('county')
         }
+        return location_map
+
+    @view_config(name='',
+                 context=LocationFactory,
+                 renderer='counties_list.jinja2',
+                 request_method='GET')
+    def show_all(self):
+        filter_criteria = Project.generate_filter_criteria()
+        view_by = self.request.GET.get('view_by')
+        location_map = self.get_location_map()
+
         impact_indicator_results = generate_impact_indicators_for(location_map)
         aggregate_type = impact_indicator_results['aggregate_type']
         location = impact_indicator_results['location']
+
         if aggregate_type is 'Project':
             dataset = build_dataset(
                 aggregate_type,
@@ -96,6 +100,9 @@ class CountyView(object):
     def performance(self):
         sector_indicator_mapping = {}
         sector_aggregated_indicators = {}
+        location_map = self.get_location_map()
+        view_by = self.request.GET.get('view_by')
+
         county = self.request.context
         sub_counties = SubCounty.all(SubCounty.parent_id == county.id)
         sub_county_ids = [subcounty.id for subcounty in sub_counties]
@@ -113,6 +120,9 @@ class CountyView(object):
             sector_indicator_mapping[reg_id] = indicator_mapping
             sector_aggregated_indicators[reg_id] = aggregated_indicators
         filter_criteria = Project.generate_filter_criteria()
+
+        search_criteria = {'view_by': view_by,
+                           'location_map': location_map}
         return {
             'title': county.pretty,
             'county': county,
@@ -120,7 +130,8 @@ class CountyView(object):
             'project_types': project_types_mappings,
             'sector_aggregated_indicators': sector_aggregated_indicators,
             'sector_indicator_mapping': sector_indicator_mapping,
-            'filter_criteria': filter_criteria
+            'filter_criteria': filter_criteria,
+            'search_criteria': search_criteria
         }
 
     @view_config(name='performance_summary',
@@ -132,6 +143,12 @@ class CountyView(object):
         sector_aggregated_indicators = {}
         counties = County.all()
         county_ids = [county.id for county in counties]
+
+        location_map = self.get_location_map()
+        view_by = self.request.GET.get('view_by')
+        search_criteria = {'view_by': view_by,
+                           'location_map': location_map}
+
         project_types_mappings = helpers.get_project_types(
             helpers.get_community_ids(
                 helpers.get_constituency_ids(
@@ -153,5 +170,6 @@ class CountyView(object):
             'project_types': project_types_mappings,
             'sector_aggregated_indicators': sector_aggregated_indicators,
             'sector_indicator_mapping': sector_indicator_mapping,
-            'filter_criteria': filter_criteria
+            'filter_criteria': filter_criteria,
+            'search_criteria': search_criteria
         }
