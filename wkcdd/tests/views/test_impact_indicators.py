7ef8f901160fa1ff3b2c73a21e1e1cac4c3e13dc
geoffreymuchai <geoffreymuchai@gmail.com>
2014-05-06 14:26:21 +0300

    GM: Added more view tests to impact indicators

--- wkcdd/tests/views/test_impact_indicators.py
+++ wkcdd/tests/views/test_impact_indicators.py
@@ -1,5 +1,5 @@
 from wkcdd.tests.test_base import FunctionalTestBase
-from wkcdd.models import Community
+from wkcdd.models import Community, County, Constituency
 
 
 class TestImpactIndicatorsFunctional(FunctionalTestBase):
@@ -14,5 +14,22 @@ class TestImpactIndicatorsFunctional(FunctionalTestBase):
         community = Community.get(Community.name == "Maragoli")
         url = self.request.route_path('impact_indicators',
                                       traverse=(community.id))
+        response = self.testapp.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_county_projects(self):
+        self.setup_test_data()
+        county = County.get(County.name == "Bungoma")
+        url = self.request.route_path('impact_indicators',
+                                      traverse=(county.id),
+                                      _query={'view_by': 'projects'})
+        response = self.testapp.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_constituency_communities(self):
+        self.setup_test_data()
+        constituency = Constituency.get(Constituency.name == "Amagoro")
+        url = self.request.route_path('impact_indicators',
+                                      traverse=(constituency.id))
         response = self.testapp.get(url)
         self.assertEqual(response.status_code, 200)
