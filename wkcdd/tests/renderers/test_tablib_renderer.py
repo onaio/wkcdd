ad83bd693c42751afefa38562af86e3d7eb1f355
geoffreymuchai <geoffreymuchai@gmail.com>
2014-05-16 15:57:41 +0300

    GM: Added performance indicator xlsx exports

--- wkcdd/tests/renderers/test_tablib_renderer.py
+++ wkcdd/tests/renderers/test_tablib_renderer.py
@@ -3,6 +3,7 @@ import unittest
 from pyramid import testing
 from wkcdd.renderers import TablibXLSXRenderer
 from wkcdd.libs.utils import get_impact_indicator_list
+from wkcdd.views.helpers import get_performance_sector_mapping
 from wkcdd import constants
 from wkcdd.models import County
 
@@ -29,6 +30,50 @@ class TestTablibRenderer(unittest.TestCase):
                             'impact_information/b_improved_houses': 0,
                             'impact_information/b_hh_assets': 0,
                             'impact_information/b_income': 0}
+        }
+        renderer(data, {'request': request})
+        self.assertEqual(
+            request.response.content_type,
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')  # noqa
+
+    def test_renderer_output_for_performance_indicators(self):
+        request = testing.DummyRequest()
+        renderer = TablibXLSXRenderer({})
+        sectors = get_performance_sector_mapping()
+        goat_reg_id = constants.DAIRY_GOAT_PROJECT_REGISTRATION
+        goat_sector_data = {'rows': [
+            {'indicators': {
+                            'exp_contribution': 0,
+                            'actual_contribution': 0,
+                            'community_contribution': 0,
+                            'bucks_target': 0,
+                            'bucks_achievement': 0,
+                            'bucks_percentage': 0},
+             'location': County(name="bla", id=1)
+             }],
+            'summary_row': {'exp_contribution': 0,
+                            'actual_contribution': 0,
+                            'community_contribution': 0,
+                            'bucks_target': 0,
+                            'bucks_achievement': 0,
+                            'bucks_percentage': 0}}
+        data = {
+            'sectors': sectors,
+            'is_impact': False,
+            'sector_indicators': {
+                goat_reg_id: (
+                    ('Community Contribution', (
+                        'exp_contribution',
+                        'actual_contribution',
+                        'community_contribution')),
+                    ('Local Bucks Acquired', (
+                        'bucks_target',
+                        'bucks_achievement',
+                        'bucks_percentage')))
+            },
+            'sector_data': {
+                goat_reg_id: goat_sector_data},
+            'search_criteria': {'selected_sector': {'sector': goat_reg_id}}
         }
         renderer(data, {'request': request})
         self.assertEqual(
