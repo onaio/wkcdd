6368e9463deb715ff9f57dea4fcae649c95d85fe
geoffreymuchai <geoffreymuchai@gmail.com>
2014-05-19 15:37:37 +0300

    GM:Set chart to show only data for selected indicator

--- wkcdd/views/helpers.py
+++ wkcdd/views/helpers.py
@@ -181,15 +181,15 @@ def build_impact_indicator_chart_dataset(indicators, rows):
     Generate JSON dataset
         {
             labels: ['location a', 'location b', 'location c'],
-            series: [[1,2,3,4], [5,6,7,8], [9,10,11,12]],
-            series_labels: ['Increased income', 'Improved households']
+            series: { 'increased_income': [1,2,3,4],
+                      'improved_households': [5,6,7,8]
+                    }
         }
     """
     dataset = {}
-    series_labels = [item['label'] for item in indicators]
     indicator_keys = [item['key'] for item in indicators]
     labels = [row['location'].pretty for row in rows]
-    series = []
+    series = {}
 
     for key in indicator_keys:
         indicator_series = []
@@ -198,11 +198,10 @@ def build_impact_indicator_chart_dataset(indicators, rows):
             if row['indicators']:
                 indicator_series.append(row['indicators'][key])
 
-        series.append(indicator_series)
+        series[key] = indicator_series
 
     dataset['labels'] = labels
     dataset['series'] = series
-    dataset['series_labels'] = series_labels
 
     return json.dumps(dataset)
 
@@ -212,19 +211,18 @@ def build_performance_indicator_chart_dataset(indicators, rows):
     Generate JSON dataset
         {
             labels: ['location a', 'location b', 'location c'],
-            series: [[1%,2%,3%,4%], [5%,6%,7%,8%], [9%,10%,11%,12%]],
-            series_labels: ['sector label a', 'sector label b']
+            series: { 'sector label a': [1%,2%,3%,4%],
+                      'sector label b': [5%,6%,7%,8%]
+                    }
         }
     """
     dataset = {}
 
     # Exclude labels that have no ratio field
-    series_labels = [label
-                     for label, key_group in indicators if key_group[2]]
     indicator_keys = [key_group[2]
                       for label, key_group in indicators if key_group[2]]
     labels = [row['location'].pretty for row in rows]
-    series = []
+    series = {}
 
     for key in indicator_keys:
         indicator_series = []
@@ -233,10 +231,9 @@ def build_performance_indicator_chart_dataset(indicators, rows):
             if row['indicators']:
                 indicator_series.append(row['indicators'][key])
 
-        series.append(indicator_series)
+        series[key] = indicator_series
 
     dataset['labels'] = labels
     dataset['series'] = series
-    dataset['series_labels'] = series_labels
 
     return json.dumps(dataset)
