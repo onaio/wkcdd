0110198360f5680ac71d1ebc297125f2f0a91748
William <willyk99@gmail.com>
2014-05-26 23:14:26 +0300

    WM: Test for responsive datatable

--- wkcdd/static/scripts/datatable.js
+++ wkcdd/static/scripts/datatable.js
@@ -1,6 +1,27 @@
-$(document).ready(function(){
-    $("#prjDatatable").dataTable();
-    $("#prjDatatable input").addClass(".form-control");
-    
-    $(".wk-datatable").dataTable();
+$(document).ready(function () {
+    var responsiveHelper = undefined;
+    var breakpointDefinition = {
+        tablet: 1024,
+        phone : 480
+    };
+    var tableElement = $('.wk-datatable');
+
+    tableElement.dataTable({
+        autoWidth        : false,
+        preDrawCallback: function () {
+            // Initialize the responsive datatables helper once.
+            if (!responsiveHelper) {
+                responsiveHelper = new ResponsiveDatatablesHelper(tableElement, breakpointDefinition);
+            }
+        },
+        rowCallback    : function (nRow) {
+            responsiveHelper.createExpandIcon(nRow);
+        },
+        drawCallback   : function (oSettings) {
+            responsiveHelper.respond();
+        }
+    });
+    // $("#prjDatatable").dataTable();
+    // $("#prjDatatable input").addClass(".form-control");
+    // $(".wk-datatable").dataTable();
 });
