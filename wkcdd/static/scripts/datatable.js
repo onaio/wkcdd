f7d031d2709a7b5370159bd45972f5a1859f3192
William <willyk99@gmail.com>
2014-05-29 17:57:28 +0300

    WM: No conflict jquery added for datatables

--- wkcdd/static/scripts/datatable.js
+++ wkcdd/static/scripts/datatable.js
@@ -1,28 +1,32 @@
 'use strict';
 
-$(document).ready(function () {
+var jq10 = jQuery.noConflict();
+
+jq10(document).ready(function () {
     var responsiveHelper = undefined;
     var breakpointDefinition = {
         tablet: 1024,
         phone : 480
     };
+    
     var tableElement = $('.wk-datatable');
 
-    tableElement.dataTable({
-        autoWidth        : true,
-        preDrawCallback: function () {
-            // Initialize the responsive datatables helper once.
-            if (!responsiveHelper) {
-                responsiveHelper = new ResponsiveDatatablesHelper(tableElement, breakpointDefinition);
-            }
-        },
-        rowCallback    : function (nRow) {
-            responsiveHelper.createExpandIcon(nRow);
-        },
-        drawCallback   : function (oSettings) {
-            responsiveHelper.respond();
-        }
-    });
+    // tableElement.dataTable({
+        // autoWidth        : true,
+        // preDrawCallback: function () {
+            // // Initialize the responsive datatables helper once.
+            // if (!responsiveHelper) {
+                // responsiveHelper = new ResponsiveDatatablesHelper(tableElement, breakpointDefinition);
+            // }
+        // },
+        // rowCallback    : function (nRow) {
+            // responsiveHelper.createExpandIcon(nRow);
+        // },
+        // drawCallback   : function (oSettings) {
+            // responsiveHelper.respond();
+        // }
+    // });
     
+    tableElement.dataTable();
     tableElement.find("input").addClass(".form-control");
 });
