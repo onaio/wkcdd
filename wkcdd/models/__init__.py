66828e340e4f60f87b5cd051486b235f6d0f3092
geoffreymuchai <geoffreymuchai@gmail.com>
2014-07-30 08:44:44 +0300

    GM: Fixed failing Pep8 test

--- wkcdd/models/__init__.py
+++ wkcdd/models/__init__.py
@@ -3,8 +3,8 @@ from wkcdd.models.constituency import Constituency  # noqa
 from wkcdd.models.county import County  # noqa
 from wkcdd.models.form import Form  # noqa
 from wkcdd.models.location import Location  # noqa
-from wkcdd.models.meeting import MeetingReport # noqa
-from wkcdd.models.meeting import SaicMeetingReport # noqa
+from wkcdd.models.meeting import MeetingReport  # noqa
+from wkcdd.models.meeting import SaicMeetingReport  # noqa
 
 from wkcdd.models.project import Project  # noqa
 from wkcdd.models.report import Report  # noqa
