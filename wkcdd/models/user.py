7f0c7748cef74332d614d874789a5690e10828c0
geoffreymuchai <geoffreymuchai@gmail.com>
2014-08-04 12:12:45 +0300

    GM: Added ability to login and logout users

--- wkcdd/models/user.py
+++ wkcdd/models/user.py
@@ -34,3 +34,11 @@ class User(Base):
         self.pwd = pwd_context.encrypt(value)
 
     password = synonym('pwd', descriptor=password)
+
+    def check_password(self, password):
+        from wkcdd.security import pwd_context
+        # always return false if password is greater than 255 to avoid
+        # spoofing attacks
+        if len(password) > 255:
+            return False
+        return pwd_context.verify(password, self.pwd)
