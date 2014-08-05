09a70f9f604e1fc4e4b23af275c9563e9d0bd074
geoffreymuchai <geoffreymuchai@gmail.com>
2014-08-05 18:01:40 +0300

    GM: Restricted options available to logged in user based on user group; fixed failing test

--- wkcdd/tests/views/test_private_view.py
+++ wkcdd/tests/views/test_private_view.py
@@ -3,6 +3,7 @@ from pyramid import testing
 from wkcdd.tests.test_base import (
     IntegrationTestBase,
     FunctionalTestBase)
+from wkcdd.models.user import User
 from wkcdd.views.default_views import private
 
 
@@ -15,7 +16,9 @@ class TestPrivateView(IntegrationTestBase):
 
 class TestHomeViewFunctional(FunctionalTestBase):
     def test_private_view_response(self):
+        self._create_user()
+        user = User.newest()
         url = self.request.route_url('private')
-        auth_headers = self._login_user(1)
+        auth_headers = self._login_user(user)
         response = self.testapp.get(url, headers=auth_headers)
         self.assertEqual(response.status_code, 200)
